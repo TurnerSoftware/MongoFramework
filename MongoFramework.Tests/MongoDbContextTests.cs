d3a9c723fc1106c74f7b0b3ccdc63efc84479dd1
Turnerj <git@turnerj.com>
2018-01-21 23:18:14 +1030

    Added DbSet and DbContext tests

---
+++ MongoFramework.Tests/MongoDbContextTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoFramework.Tests
+{
+	public class MongoDbContextTestContext : MongoDbContext
+	{
+		public MongoDbContextTestContext(string connectionString, string databaseName) : base(connectionString, databaseName) { }
+
+		public MongoDbSet<MongoDbContextModel> ContextDbSet { get; set; }
+	}
+
+	public class MongoDbContextModel
+	{
+		public string Id { get; set; }
+	}
+
+	[TestClass]
+	public class MongoDbContextTests
+	{
+		public void ContextCreatesDbSets()
+		{
+			var connectionString = TestConfiguration.ConnectionString;
+			var databaseName = TestConfiguration.GetDatabaseName();
+			using (var context = new MongoDbContextTestContext(connectionString, databaseName))
+			{
+				Assert.IsNotNull(context.ContextDbSet);
+			}
+		}
+
+		public void ContextSavesDbSets()
+		{
+			var connectionString = TestConfiguration.ConnectionString;
+			var databaseName = TestConfiguration.GetDatabaseName();
+			using (var context = new MongoDbContextTestContext(connectionString, databaseName))
+			{
+				context.ContextDbSet.Add(new MongoDbContextModel());
+				Assert.IsFalse(context.ContextDbSet.Any());
+				context.SaveChanges();
+				Assert.IsTrue(context.ContextDbSet.Any());
+			}
+		}
+	}
+}
---
+++ MongoFramework.Tests/MongoDbSetTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoFramework.Tests
+{
+	public class MongoDbSetValidationModel
+	{
+		public string Id { get; set; }
+
+		[Required]
+		public string RequiredField { get; set; }
+	}
+
+	[TestClass]
+	public class MongoDbSetTests
+	{
+		[TestMethod]
+		public void InitialiseDbSet()
+		{
+			var connectionString = TestConfiguration.ConnectionString;
+			var databaseName = TestConfiguration.GetDatabaseName();
+			AssertExtensions.DoesNotThrow<Exception>(() => new MongoDbSet<MongoDbSetValidationModel>(connectionString, databaseName));
+		}
+
+		[TestMethod, ExpectedException(typeof(ValidationException))]
+		public void ValidationExceptionOnInvalidModel()
+		{
+			var database = TestConfiguration.GetDatabase();
+			var dbSet = new MongoDbSet<MongoDbSetValidationModel>();
+			dbSet.SetDatabase(database);
+			
+			dbSet.Add(new MongoDbSetValidationModel());
+			dbSet.SaveChanges();
+		}
+
+		[TestMethod]
+		public void SuccessfulInsertAndQueryBack()
+		{
+			var database = TestConfiguration.GetDatabase();
+			var dbSet = new MongoDbSet<MongoDbSetValidationModel>();
+			dbSet.SetDatabase(database);
+
+			dbSet.Add(new MongoDbSetValidationModel
+			{
+				RequiredField = "ValueSync"
+			});
+
+			Assert.IsFalse(dbSet.Any(m => m.RequiredField == "ValueSync"));
+			dbSet.SaveChanges();
+			Assert.IsTrue(dbSet.Any(m => m.RequiredField == "ValueSync"));
+		}
+
+		[TestMethod]
+		public async Task SuccessfulInsertAndQueryBackAsync()
+		{
+			var database = TestConfiguration.GetDatabase();
+			var dbSet = new MongoDbSet<MongoDbSetValidationModel>();
+			dbSet.SetDatabase(database);
+
+			dbSet.Add(new MongoDbSetValidationModel
+			{
+				RequiredField = "ValueAsync"
+			});
+
+			Assert.IsFalse(dbSet.Any(m => m.RequiredField == "ValueAsync"));
+			await dbSet.SaveChangesAsync();
+			Assert.IsTrue(dbSet.Any(m => m.RequiredField == "ValueAsync"));
+		}
+	}
+}
