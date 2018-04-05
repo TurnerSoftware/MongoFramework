5cf6558c25e564463f7c0d1655df9c187bf1fc0f
Turnerj <git@turnerj.com>
2018-04-05 22:48:00 +0930

    Additional mutator test

---
+++ MongoFramework.Tests/EntityRelationships/NavigationPropertyMutatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoFramework.Infrastructure.Mutation.Mutators;
+
+namespace MongoFramework.Tests.EntityRelationships
+{
+	[TestClass]
+	public class NavigationPropertyMutatorTests
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void MutatorRequiresDatabase()
+		{
+			var mutator = new NavigationPropertyMutator<GuidIdModel>();
+			mutator.MutateEntity(new GuidIdModel(), Infrastructure.Mutation.MutatorType.Create, null, null);
+		}
+	}
+}
