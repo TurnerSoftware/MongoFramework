1f49d2315acb8c1c28c9b3a0177988473c9be1bc
Turnerj <git@turnerj.com>
2018-04-25 15:56:52 +0930

    And this is why we build before committing...

--- MongoFramework.Tests/Infrastructure/EntityRelationships/EntityNavigationCollectionUnitTests.cs
+++ MongoFramework.Tests/Infrastructure/EntityRelationships/EntityNavigationCollectionUnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MongoDB.Bson;
+using MongoFramework.Infrastructure.EntityRelationships;
 using System;
 using System.Linq;
 
@@ -11,20 +12,20 @@ namespace MongoFramework.Tests.Infrastructure.EntityRelationships
 		[TestMethod, ExpectedException(typeof(ArgumentNullException))]
 		public void NullForeignKeyThrowsException()
 		{
-			new Infrastructure.EntityRelationships.EntityNavigationCollection<StringIdModel>(null);
+			new EntityNavigationCollection<StringIdModel>(null);
 		}
 
 		[TestMethod, ExpectedException(typeof(ArgumentNullException))]
 		public void AddForeignIdWithNull()
 		{
-			var collection = new Infrastructure.EntityRelationships.EntityNavigationCollection<StringIdModel>("Id");
+			var collection = new EntityNavigationCollection<StringIdModel>("Id");
 			collection.AddForeignId(null);
 		}
 
 		[TestMethod]
 		public void AddForeignIdWithRightType()
 		{
-			var collection = new Infrastructure.EntityRelationships.EntityNavigationCollection<StringIdModel>("Id");
+			var collection = new EntityNavigationCollection<StringIdModel>("Id");
 			collection.AddForeignId("12345678");
 			Assert.AreEqual(1, collection.UnloadedCount);
 		}
@@ -32,14 +33,14 @@ namespace MongoFramework.Tests.Infrastructure.EntityRelationships
 		[TestMethod, ExpectedException(typeof(InvalidOperationException))]
 		public void AddForeignIdWithWrongType()
 		{
-			var collection = new Infrastructure.EntityRelationships.EntityNavigationCollection<StringIdModel>("Id");
+			var collection = new EntityNavigationCollection<StringIdModel>("Id");
 			collection.AddForeignId(ObjectId.GenerateNewId());
 		}
 
 		[TestMethod]
 		public void AddMultipleForeignIds()
 		{
-			var collection = new Infrastructure.EntityRelationships.EntityNavigationCollection<ObjectIdIdModel>("Id");
+			var collection = new EntityNavigationCollection<ObjectIdIdModel>("Id");
 			collection.AddForeignIds(new object[] { ObjectId.GenerateNewId(), ObjectId.GenerateNewId() });
 			Assert.AreEqual(2, collection.UnloadedCount);
 		}
