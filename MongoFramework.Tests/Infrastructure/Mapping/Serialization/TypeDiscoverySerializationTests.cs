a8c4a6bc7ade922fdb980f67eb8b1fafbebab4c2
Turnerj <git@turnerj.com>
2018-08-11 14:52:09 +0930

    Removed old reset code for Type Discovery tests

--- MongoFramework.Tests/Infrastructure/Mapping/Serialization/TypeDiscoverySerializationTests.cs
+++ MongoFramework.Tests/Infrastructure/Mapping/Serialization/TypeDiscoverySerializationTests.cs
@@ -34,30 +34,6 @@ namespace MongoFramework.Tests.Infrastructure.Mapping.Serialization
 		{
 		}
 
-		[TestInitialize]
-		public void ResetMongoDbDriver()
-		{
-			var classMapField = typeof(BsonClassMap).GetField("__classMaps", BindingFlags.NonPublic | BindingFlags.Static);
-			classMapField.SetValue(null, new Dictionary<Type, BsonClassMap>());
-
-			var knownTypesField = typeof(BsonSerializer).GetField("__typesWithRegisteredKnownTypes", BindingFlags.NonPublic | BindingFlags.Static);
-			knownTypesField.SetValue(null, new HashSet<Type>());
-
-			var discriminatorTypesField = typeof(BsonSerializer).GetField("__discriminatedTypes", BindingFlags.NonPublic | BindingFlags.Static);
-			discriminatorTypesField.SetValue(null, new HashSet<Type>());
-
-			var discriminatorsField = typeof(BsonSerializer).GetField("__discriminators", BindingFlags.NonPublic | BindingFlags.Static);
-			discriminatorsField.SetValue(null, new Dictionary<BsonValue, HashSet<Type>>());
-
-			var serializerRegistryField = typeof(BsonSerializer).GetField("__serializerRegistry", BindingFlags.NonPublic | BindingFlags.Static);
-			if (serializerRegistryField.GetValue(null) is BsonSerializerRegistry registry)
-			{
-				var cacheField = typeof(BsonSerializerRegistry).GetField("_cache", BindingFlags.NonPublic | BindingFlags.Instance);
-				var registryCache = cacheField.GetValue(registry) as ConcurrentDictionary<Type, IBsonSerializer>;
-				registryCache.Clear();
-			}
-		}
-
 		[TestMethod]
 		public void NullDeserialization()
 		{
