3e353ad36b506c1b77b0326ced3e58a32a79b0aa
Turnerj <git@turnerj.com>
2018-12-09 17:19:43 +1030

    Added additional MongoDbConnection tests

---
+++ MongoFramework.Tests/MongoDbConnectionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MongoFramework.Tests
+{
+	[TestClass]
+	public class MongoDbConnectionTests
+	{
+#if !NETCOREAPP2_0
+		[TestMethod]
+		public void ConnectionFromConfig()
+		{
+			var connection = MongoDbConnection.FromConfig("MongoFrameworkTests");
+			Assert.IsNotNull(connection);
+		}
+		[TestMethod]
+		public void InvalidConfigForConnection()
+		{
+			var connection = MongoDbConnection.FromConfig("ThisConfigNameDoesntExist");
+			Assert.IsNull(connection);
+		}
+#endif
+
+		[TestMethod, ExpectedException(typeof(ArgumentNullException))]
+		public void NullUrlThrowsException()
+		{
+			MongoDbConnection.FromUrl(null);
+		}
+	}
+}
