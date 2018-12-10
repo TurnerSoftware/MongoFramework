45df7bd6d30f2b4b657c13fbd6e6250645135661
Turnerj <git@turnerj.com>
2018-12-10 17:49:49 +1030

    Added tests for MiniProfilerDiagnosticListener

---
+++ MongoFramework.Tests/Profiling/MiniProfiler/MiniProfilerDiagnosticListenerTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoFramework.Attributes;
+using MongoFramework.Profiling.MiniProfiler;
+using SEProfiling = StackExchange.Profiling;
+
+namespace MongoFramework.Tests.Profiling.MiniProfiler
+{
+	[TestClass]
+	public class MiniProfilerDiagnosticListenerTests : TestBase
+	{
+		private IMongoDbConnection GetConnection()
+		{
+			var connection = MongoDbConnection.FromConnectionString(TestConfiguration.ConnectionString, TestConfiguration.GetDatabaseName());
+			connection.DiagnosticListener = new MiniProfilerDiagnosticListener();
+			return connection;
+		}
+
+		class TestContext : MongoDbContext
+		{
+			public TestContext(IMongoDbConnection connection) : base(connection) { }
+
+			public MongoDbSet<Person> People { get; set; }
+		}
+
+		class Person
+		{
+			public string Id { get; set; }
+			public string Name { get; set; }
+			public int Age { get; set; }
+		}
+
+		[TestMethod]
+		public void ProfilingInsert()
+		{
+			using (var context = new TestContext(GetConnection()))
+			{
+				var profiler = SEProfiling.MiniProfiler.StartNew();
+				context.People.Add(new Person
+				{
+					Name = "ProfilingInsert",
+				});
+				context.SaveChanges();
+
+				Assert.IsTrue(profiler.Root.CustomTimings.ContainsKey("mongoframework"));
+				var timings = profiler.Root.CustomTimings["mongoframework"];
+				Assert.IsTrue(timings[0].CommandString.Contains("InsertOne"));
+				Assert.IsTrue(timings[0].CommandString.Contains("ProfilingInsert"));
+			}
+		}
+
+		[TestMethod]
+		public void ProfilingUpdate()
+		{
+			using (var context = new TestContext(GetConnection()))
+			{
+				var entity = new Person
+				{
+					Name = "ProfilingUpdate",
+				};
+				context.People.Add(entity);
+				context.SaveChanges();
+
+				var profiler = SEProfiling.MiniProfiler.StartNew();
+
+				entity.Name = "ProfilingUpdate-Updated";
+				context.SaveChanges();
+
+				Assert.IsTrue(profiler.Root.CustomTimings.ContainsKey("mongoframework"));
+				var timings = profiler.Root.CustomTimings["mongoframework"];
+				Assert.IsTrue(timings[0].CommandString.Contains("UpdateOne"));
+				Assert.IsTrue(timings[0].CommandString.Contains("ProfilingUpdate-Updated"));
+			}
+		}
+
+		[TestMethod]
+		public void ProfilingDelete()
+		{
+			using (var context = new TestContext(GetConnection()))
+			{
+				var entity = new Person
+				{
+					Name = "ProfilingDelete",
+				};
+				context.People.Add(entity);
+				context.SaveChanges();
+
+				var profiler = SEProfiling.MiniProfiler.StartNew();
+
+				context.People.Remove(entity);
+				context.SaveChanges();
+
+				Assert.IsTrue(profiler.Root.CustomTimings.ContainsKey("mongoframework"));
+				var timings = profiler.Root.CustomTimings["mongoframework"];
+				Assert.IsTrue(timings[0].CommandString.Contains("DeleteOne"));
+				Assert.IsTrue(timings[0].CommandString.Contains(entity.Id));
+			}
+		}
+
+		[TestMethod]
+		public void ProfilingRead()
+		{
+			using (var context = new TestContext(GetConnection()))
+			{
+				for (int i = 0, l = 100; i < l; i++)
+				{
+					context.People.Add(new Person
+					{
+						Age = i,
+						Name = "ProfilingRead"
+					});
+				}
+				
+				context.SaveChanges();
+
+				var profiler = SEProfiling.MiniProfiler.StartNew();
+
+				var queryable = context.People.Skip(78);
+				foreach (var entity in queryable)
+				{ }
+
+				Assert.IsTrue(profiler.Root.CustomTimings.ContainsKey("mongoframework"));
+				var timings = profiler.Root.CustomTimings["mongoframework"];
+				Assert.IsTrue(timings[0].CommandString.Contains("$skip"));
+				Assert.IsTrue(timings[0].CommandString.Contains("78"));
+			}
+		}
+	}
+}
