9548b9a48a1da2c459d9af134732a8a09170d427
Turnerj <git@turnerj.com>
2019-05-26 18:11:59 +0930

    Changed how coverage tests are run

--- build.ps1
+++ build.ps1
@@ -16,7 +16,7 @@ Write-Host "Environment:" -ForegroundColor Cyan
 Write-Host "  .NET Version:" (dotnet --version)
 Write-Host "  Artifact Path: $packageOutputFolder"
 
-Write-Host "Building solution... " -ForegroundColor "Magenta"
+Write-Host "Building solution..." -ForegroundColor "Magenta"
 dotnet build -c Release
 if ($LastExitCode -ne 0) {
     Write-Host "Build failed, aborting!" -Foreground "Red"
@@ -24,8 +24,8 @@ if ($LastExitCode -ne 0) {
 }
 Write-Host "Solution built!" -ForegroundColor "Green"
 
-if ($RunTests) {
-    Write-Host "Running tests... " -ForegroundColor "Magenta"
+if ($RunTests -And -Not $CheckCoverage) {
+    Write-Host "Running tests without coverage..." -ForegroundColor "Magenta"
 	dotnet test tests/MongoFramework.Tests/MongoFramework.Tests.csproj
 	if ($LastExitCode -ne 0) {
         Write-Host "Tests failed, aborting build!" -Foreground "Red"
@@ -33,15 +33,16 @@ if ($RunTests) {
     }
 	Write-Host "Tests passed!" -ForegroundColor "Green"
 }
-
-if ($CheckCoverage) {
-	Write-Host "Checking code coverage... " -ForegroundColor "Magenta"
+elseif ($RunTests -And $CheckCoverage) {
+	Write-Host "Running tests with coverage..." -ForegroundColor "Magenta"
 	OpenCover.Console.exe -register:user -target:"%LocalAppData%\Microsoft\dotnet\dotnet.exe" -targetargs:"test tests/MongoFramework.Tests/MongoFramework.Tests.csproj /p:DebugType=Full" -filter:"+[MongoFramework]* -[MongoFramework.Tests]*" -output:"$packageOutputFolder\coverage.xml" -oldstyle
     if ($LastExitCode -ne 0 -Or -Not $?) {
-        Write-Host "Failure checking code coverage!" -Foreground "Red"
+        Write-Host "Failure performing tests with coverage, aborting!" -Foreground "Red"
+		Exit 1
     }
 	else {
-		Write-Host "Saving code coverage... " -ForegroundColor "Magenta"
+		Write-Host "Tests passed!" -ForegroundColor "Green"
+		Write-Host "Saving code coverage..." -ForegroundColor "Magenta"
 		codecov -f "$packageOutputFolder\coverage.xml"
 		if ($LastExitCode -ne 0 -Or -Not $?) {
 			Write-Host "Failure saving code coverage!" -Foreground "Red"
@@ -57,7 +58,7 @@ if ($CreatePackages) {
     Get-ChildItem $packageOutputFolder | Remove-Item
     Write-Host "Packages cleared!" -ForegroundColor "Green"
 	
-    Write-Host "Packing... " -ForegroundColor "Magenta"
+    Write-Host "Packing..." -ForegroundColor "Magenta"
 	dotnet pack --no-build -c Release /p:PackageOutputPath=$packageOutputFolder
 	Write-Host "Packing complete!" -ForegroundColor "Green"
 }
