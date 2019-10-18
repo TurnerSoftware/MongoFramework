273d441b7b3ea121e50aaf86060f1b93b7293d4f
Turnerj <git@turnerj.com>
2019-10-18 18:49:36 +1030

    Added entity mapping benchmark

---
+++ tests/MongoFramework.Benchmarks/Infrastructure/Mapping/EntityMappingBenchmark.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoFramework.Infrastructure.Mapping;
+
+namespace MongoFramework.Benchmarks.Infrastructure.Mapping
+{
+	[CoreJob, MemoryDiagnoser]
+	public class EntityMappingBenchmark
+	{
+		public class PersonModel
+		{
+			public ObjectId Id { get; set; }
+			public string Name { get; set; }
+			public DateTime DateOfBirth { get; set; }
+			public List<AddressModel> Addresses { get; set; }
+		}
+		public class AddressModel
+		{
+			public string Street { get; set; }
+			public string Suburb { get; set; }
+			public string State { get; set; }
+			public string Country { get; set; }
+		}
+
+		[Benchmark]
+		public void ResetOverhead()
+		{
+			EntityMapping.RemoveAllDefinitions();
+			MongoDbDriverHelper.ResetDriver();
+		}
+
+		[Benchmark]
+		public void ClassMapAutoMap()
+		{
+			new BsonClassMap<PersonModel>().AutoMap();
+			MongoDbDriverHelper.ResetDriver();
+		}
+
+		[Benchmark]
+		public void EntityMappingRegister()
+		{
+			EntityMapping.RegisterType(typeof(PersonModel));
+			EntityMapping.RemoveAllDefinitions();
+			MongoDbDriverHelper.ResetDriver();
+		}
+	}
+}
