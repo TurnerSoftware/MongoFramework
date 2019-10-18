624583462f261db59bfe51f35c6dca2f257a2e5a
Turnerj <git@turnerj.com>
2019-10-18 17:21:12 +1030

    Moved MongoDB Driver reset rules to separate class

--- tests/MongoFramework.Tests/TestBase.cs
+++ tests/MongoFramework.Tests/TestBase.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using MongoFramework.Infrastructure.Mapping;
 using MongoFramework.Infrastructure.Serialization;
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Reflection;
 
 namespace MongoFramework.Tests
 {
@@ -16,52 +11,13 @@ namespace MongoFramework.Tests
 	{
 		protected static void ResetMongoDb()
 		{
-			ResetMongoDbDriver();
+			MongoDbDriverHelper.ResetDriver();
 			EntityMapping.RemoveAllDefinitions();
 
 			EntityMapping.RemoveAllMappingProcessors();
 			EntityMapping.AddMappingProcessors(DefaultMappingPack.Instance.Processors);
 
 			TypeDiscovery.ClearCache();
-		}
-
-		private static void ResetMongoDbDriver()
-		{
-			//Primarily introduced to better test TypeDiscoverySerializer, this is designed to reset the MongoDB driver
-			//as if the assembly just loaded. It is likely incomplete and would be easily subject to breaking in future 
-			//driver updates. If someone knows a better way to reset the MongoDB driver, please open a pull request!
-
-			var classMapField = typeof(BsonClassMap).GetField("__classMaps", BindingFlags.NonPublic | BindingFlags.Static);
-			if (classMapField.GetValue(null) is Dictionary<Type, BsonClassMap> classMaps)
-			{
-				classMaps.Clear();
-			}
-
-			var knownTypesField = typeof(BsonSerializer).GetField("__typesWithRegisteredKnownTypes", BindingFlags.NonPublic | BindingFlags.Static);
-			if (knownTypesField.GetValue(null) is HashSet<Type> knownTypes)
-			{
-				knownTypes.Clear();
-			}
-
-			var discriminatorTypesField = typeof(BsonSerializer).GetField("__discriminatedTypes", BindingFlags.NonPublic | BindingFlags.Static);
-			if (discriminatorTypesField.GetValue(null) is HashSet<Type> discriminatorTypes)
-			{
-				discriminatorTypes.Clear();
-			}
-
-			var discriminatorsField = typeof(BsonSerializer).GetField("__discriminators", BindingFlags.NonPublic | BindingFlags.Static);
-			if (discriminatorsField.GetValue(null) is Dictionary<BsonValue, HashSet<Type>> discriminators)
-			{
-				discriminators.Clear();
-			}
-
-			var serializerRegistryField = typeof(BsonSerializer).GetField("__serializerRegistry", BindingFlags.NonPublic | BindingFlags.Static);
-			if (serializerRegistryField.GetValue(null) is BsonSerializerRegistry registry)
-			{
-				var cacheField = typeof(BsonSerializerRegistry).GetField("_cache", BindingFlags.NonPublic | BindingFlags.Instance);
-				var registryCache = cacheField.GetValue(registry) as ConcurrentDictionary<Type, IBsonSerializer>;
-				registryCache.Clear();
-			}
 		}
 
 		protected static void ClearDatabase()
