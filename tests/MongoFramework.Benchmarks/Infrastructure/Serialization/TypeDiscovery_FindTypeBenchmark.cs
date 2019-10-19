ea900a1136c6fb643d5abe1fe3859a6579002028
Turnerj <git@turnerj.com>
2019-10-19 21:26:51 +1030

    Added type discovery (find type) benchmark

---
+++ tests/MongoFramework.Benchmarks/Infrastructure/Serialization/TypeDiscovery_FindTypeBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using MongoFramework.Infrastructure.Serialization;
+
+namespace MongoFramework.Benchmarks.Infrastructure.Serialization
+{
+	[CoreJob, MemoryDiagnoser]
+	public class TypeDiscovery_FindTypeBenchmark
+	{
+		private class LocalClass { }
+
+		[Benchmark]
+		public void Overhead()
+		{
+			TypeDiscovery.ClearCache();
+		}
+
+		[Benchmark]
+		public void FindString()
+		{
+			TypeDiscovery.FindTypeByDiscriminator("string", typeof(object));
+			TypeDiscovery.ClearCache();
+		}
+
+		[Benchmark]
+		public void FindMongoDbContext()
+		{
+			TypeDiscovery.FindTypeByDiscriminator("MongoDbContext", typeof(object));
+			TypeDiscovery.ClearCache();
+		}
+
+		[Benchmark]
+		public void FindLocalClass()
+		{
+			TypeDiscovery.FindTypeByDiscriminator("LocalClass", typeof(object));
+			TypeDiscovery.ClearCache();
+		}
+	}
+}
