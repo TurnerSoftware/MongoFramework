46efb1b170f761f64fa06af6a65089503ac1deae
Turnerj <git@turnerj.com>
2019-11-04 23:38:25 +1030

    First pass at basic async querying support
    Note: This commit removes .NET Standard 2.0 support however that won't be the case in the future - .NET Standard 2.0 support will still exist, this was just to get some basic tests running without needing to do a ton of ifdef statements.

---
+++ src/MongoFramework/Infrastructure/Linq/ExpressionAccumulatorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MongoFramework.Infrastructure.Linq
+{
+	public static class ExpressionAccumulatorHelper
+	{
+		public static LambdaExpression AsyncResultTransformer(LambdaExpression resultTransformer)
+		{
+			var resultTransformerType = resultTransformer.Body;
+
+			return null;
+		}
+	}
+}
