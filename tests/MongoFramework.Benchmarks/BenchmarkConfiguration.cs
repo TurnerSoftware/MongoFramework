f49a577eca5932978df01ab00bdae628e58dbcfd
Turnerj <git@turnerj.com>
2020-08-17 16:10:32 +0930

    Add DbSet comparison benchmark

---
+++ tests/MongoFramework.Benchmarks/BenchmarkConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+
+namespace MongoFramework.Benchmarks
+{
+	static class BenchmarkDb
+	{
+		public static string ConnectionString => Environment.GetEnvironmentVariable("MONGODB_URI") ?? "mongodb://localhost";
+
+		public static string GetDatabaseName()
+		{
+			return "MongoFrameworkBenchmarks";
+		}
+
+		public static IMongoDbConnection GetConnection()
+		{
+			var urlBuilder = new MongoUrlBuilder(ConnectionString)
+			{
+				DatabaseName = GetDatabaseName()
+			};
+			return MongoDbConnection.FromUrl(urlBuilder.ToMongoUrl());
+		}
+
+		public static void DropDatabase()
+		{
+			GetConnection().Client.DropDatabase(GetDatabaseName());
+		}
+	}
+}
---
+++ tests/MongoFramework.Benchmarks/MongoDbSetComparisonBenchmark.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using MongoFramework.Attributes;
+
+namespace MongoFramework.Benchmarks
+{
+	[SimpleJob(RuntimeMoniker.NetCoreApp30), MemoryDiagnoser]
+	public class MongoDbSetComparisonBenchmark
+	{
+		private class TestModel
+		{
+			public string Id { get; set; }
+			[Index(IndexSortOrder.Ascending)]
+			public string Name { get; set; }
+			public DateTime Date { get; set; }
+			public int OtherReference { get; set; }
+			public NestedTestModel NestedModel { get; set; }
+		}
+
+		private class NestedTestModel
+		{
+			public string NestedName { get; set; }
+			public int NestedReference { get; set; }
+		}
+
+		private class TestBucketGroup
+		{
+			[Index(IndexSortOrder.Ascending)]
+			public string Name { get; set; }
+		}
+
+		private class TestBucketItem
+		{
+			public DateTime Date { get; set; }
+			public int OtherReference { get; set; }
+			public NestedTestModel NestedModel { get; set; }
+		}
+
+		private class CustomDbContext : MongoDbContext
+		{
+			public CustomDbContext(IMongoDbConnection connection) : base(connection) { }
+			public MongoDbSet<TestModel> TestModels { get; set; }
+			[BucketSetOptions(200, nameof(TestBucketItem.Date))]
+			public MongoDbBucketSet<TestBucketGroup, TestBucketItem> TestBucketModels { get; set; }
+		}
+
+		public int NumberOfGroups = 8;
+		public int EntitiesPerGroup = 200;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			BenchmarkDb.DropDatabase();
+		}
+
+		[GlobalCleanup]
+		public void Cleanup()
+		{
+			BenchmarkDb.DropDatabase();
+		}
+
+		[Benchmark]
+		public void MongoDbSet()
+		{
+			var connection = BenchmarkDb.GetConnection();
+			var date = new DateTime(2000, 1, 1);
+			using (var context = new CustomDbContext(connection))
+			{
+				//Add
+				for (var i = 0; i < NumberOfGroups; i++)
+				{
+					for (var j = 0; j < EntitiesPerGroup; j++)
+					{
+						var v = i * EntitiesPerGroup + j;
+						context.TestModels.Add(new TestModel
+						{
+							Date = date.AddDays(v),
+							Name = "TestModel",
+							OtherReference = i * EntitiesPerGroup + v,
+							NestedModel = new NestedTestModel
+							{
+								NestedName = $"TestModel {v}",
+								NestedReference = v
+							}
+						});
+					}
+					context.SaveChanges();
+				}
+
+				//Read
+				for (var i = 0; i < EntitiesPerGroup; i++)
+				{
+					context.TestModels
+						.Where(t => t.Name == "TestModel")
+						.Sum(t => t.OtherReference);
+				}
+
+				//Remove
+				context.TestModels.RemoveRange(t => true);
+				context.SaveChanges();
+			}
+		}
+
+		[Benchmark]
+		public void MongoDbBucketSet()
+		{
+			var connection = BenchmarkDb.GetConnection();
+			var date = new DateTime(2000, 1, 1);
+			using (var context = new CustomDbContext(connection))
+			{
+				//Add
+				for (var i = 0; i < NumberOfGroups; i++)
+				{
+					for (var j = 0; j < EntitiesPerGroup; j++)
+					{
+						var v = i * EntitiesPerGroup + j;
+						context.TestBucketModels.Add(new TestBucketGroup
+						{
+							Name = "TestModel"
+						},
+						new TestBucketItem
+						{
+							Date = date.AddDays(v),
+							OtherReference = v,
+							NestedModel = new NestedTestModel
+							{
+								NestedName = $"TestModel {v}",
+								NestedReference = v
+							}
+						});
+					}
+					context.SaveChanges();
+				}
+
+				//Read
+				for (var i = 0; i < EntitiesPerGroup; i++)
+				{
+					context.TestBucketModels.WithGroup(new TestBucketGroup
+					{
+						Name = "TestModel"
+					}).Sum(t => t.OtherReference);
+				}
+
+				//Remove
+				context.TestBucketModels.Remove(new TestBucketGroup
+				{
+					Name = "TestModel"
+				});
+				context.SaveChanges();
+			}
+		}
+	}
+}
