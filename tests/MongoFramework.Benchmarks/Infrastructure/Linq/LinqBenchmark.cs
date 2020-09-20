651badd45b3dca09e7b46185358cc1b9aa677c6a
Turnerj <git@turnerj.com>
2020-09-20 22:49:37 +0930

    Added LINQ query generation benchmark

---
+++ tests/MongoFramework.Benchmarks/Infrastructure/Linq/LinqBenchmark.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using MongoFramework.Infrastructure.Linq;
+using MongoFramework.Infrastructure.Mapping;
+
+namespace MongoFramework.Benchmarks.Infrastructure.Linq
+{
+
+	[SimpleJob(RuntimeMoniker.NetCoreApp30), MemoryDiagnoser]
+	public class LinqBenchmark
+	{
+		private IMongoDbConnection Connection { get; set; }
+
+		public class TestModel
+		{
+			public string Id { get; set; }
+		}
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			EntityMapping.TryRegisterType(typeof(TestModel), out _);
+			Connection = BenchmarkDb.GetConnection();
+
+			//Pre-JIT the benchmarks to avoid issues with benchmark
+			FirstOrDefault();
+			ToArray();
+			Count();
+			Any();
+		}
+
+		private IMongoFrameworkQueryable<TestModel> GetQueryable()
+		{
+			var provider = new MongoFrameworkQueryProvider<TestModel>(Connection);
+			return new MongoFrameworkQueryable<TestModel>(provider);
+		}
+
+		[Benchmark]
+		public TestModel FirstOrDefault()
+		{
+			return GetQueryable().FirstOrDefault();
+		}
+
+		[Benchmark]
+		public TestModel[] ToArray()
+		{
+			return GetQueryable().ToArray();
+		}
+
+		[Benchmark]
+		public int Count()
+		{
+			return GetQueryable().Count();
+		}
+
+		[Benchmark]
+		public int Any()
+		{
+			return GetQueryable().Count();
+		}
+	}
+}
