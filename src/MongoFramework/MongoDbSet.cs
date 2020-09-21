8476574e938ac560b457ca5c0739672a94c8c5bb
John Campion <john@brightshore.com>
2020-09-21 10:14:28 -0400

    Updated comment to reflect single id

--- src/MongoFramework/MongoDbSet.cs
+++ src/MongoFramework/MongoDbSet.cs
@@ -35,7 +35,7 @@ namespace MongoFramework
 		}
 
 		/// <summary>
-		///     Finds an entity with the given primary key values. If an entity with the given primary key values
+		///     Finds an entity with the given primary key value. If an entity with the given primary key value
 		///     is being tracked by the context, then it is returned immediately without making a request to the
 		///     database. Otherwise, a query is made to the database for an entity with the given primary key value
 		///     and this entity, if found, is attached to the context and returned. If no entity is found, then
