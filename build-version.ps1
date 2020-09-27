8a07f35e7ccf630e9a444369a71eeace92a2dcdf
Turnerj <git@turnerj.com>
2020-09-27 15:09:33 +0930

    Focus on building instead of MongoDB

---
+++ build-version.ps1
@@ -0,0 +1,21 @@
+$baseBuildVersion = git describe --tags --abbrev=0
+if ($baseBuildVersion -contains "fatal") {
+	$baseBuildVersion = "0.0.0"
+}
+
+$isTagBuild = $False
+$buildMetadata = "$($env:GITHUB_SHA.substring(0,7))-$($env:GITHUB_RUN_ID)"
+$prereleaseVersion = "dev"
+
+if ($env:GITHUB_REF -ne "false") {
+	$baseBuildVersion = $env:GITHUB_REF
+	$prereleaseVersion = $False
+	$isTagBuild = $True
+}
+
+$buildVersion = "$baseBuildVersion+$buildMetadata"
+if ($prereleaseVersion) {
+	$buildVersion = "$baseBuildVersion-$prereleaseVersion+$buildMetadata"
+}
+
+Set-Env BuildVersion $buildVersion
