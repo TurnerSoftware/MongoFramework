24a4f0a3d084f08270236ba9dda44a49833f57b5
Turnerj <git@turnerj.com>
2020-10-04 22:54:52 +1030

    Use separate powershell script

---
+++ .github/workflows/buildversion.ps1
@@ -0,0 +1,16 @@
+$githubRunId = $env:GITHUB_RUN_ID;
+$prNumber = $env:GITHUB_PR;
+$gitSourceVersion = git describe --tags --abbrev=7 --always 2>$1;
+$gitSourceVersionSplit = [regex]::split($gitSourceVersion, "-(?=\d+-\w+)");
+$version = $(if($gitSourceVersionSplit.length -eq 1){"0.0.0"}else{$gitSourceVersionSplit[0]});
+$commitsSinceTag = '0';
+$commitHash = $gitSourceVersionSplit[0];
+if ($gitSourceVersionSplit.length -eq 2) {
+    $gitMetadata = $gitSourceVersionSplit[1].split("-");
+    $commitsSinceTag = $gitMetadata[0];
+    $commitHash = $gitMetadata[1];
+}
+$buildMetadata = "$($commitHash)-$($githubRunId)";
+$customSuffix = $(if($prNumber -ne ''){"-PR$($prNumber)"}elseif($commitsSinceTag -ne '0'){"-dev"});
+$version = "$($version)$($customSuffix)";
+echo "::set-env name=BuildVersion::$($version)+$($buildMetadata)";
