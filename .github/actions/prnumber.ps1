454fccfb0943a06a1f76e5cff4e96c34da03e707
Turnerj <git@turnerj.com>
2020-10-05 16:14:45 +1030

    Fixed URL for fetching PR number

--- .github/actions/prnumber.ps1
+++ .github/actions/prnumber.ps1
@@ -1,7 +1,6 @@
 $repo = $env:GITHUB_REPOSITORY;
 $commitHash = $env:GITHUB_SHA;
-$url = "https://api.github.com/repos/$($repo)/$($commitHash)/pulls";
-echo $url;
+$url = "https://api.github.com/repos/$($repo)/commits/$($commitHash)/pulls";
 $json = Invoke-WebRequest $url -Headers @{Accept = "application/vnd.github.groot-preview+json"};
 $obj = ConvertFrom-Json $json;
 echo "::set-env name=GITHUB_PR::$($obj.number)";
