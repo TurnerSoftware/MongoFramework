2e6e9d485596914cffc0124accb4334cd0d2cf67
Turnerj <git@turnerj.com>
2020-10-05 16:13:06 +1030

    Debug URL

--- .github/actions/prnumber.ps1
+++ .github/actions/prnumber.ps1
@@ -1,6 +1,7 @@
 $repo = $env:GITHUB_REPOSITORY;
 $commitHash = $env:GITHUB_SHA;
 $url = "https://api.github.com/repos/$($repo)/$($commitHash)/pulls";
+echo $url;
 $json = Invoke-WebRequest $url -Headers @{Accept = "application/vnd.github.groot-preview+json"};
 $obj = ConvertFrom-Json $json;
 echo "::set-env name=GITHUB_PR::$($obj.number)";
