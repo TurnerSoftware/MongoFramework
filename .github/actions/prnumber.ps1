88666f03da943b7787060979b008f049a4004b1c
Turnerj <git@turnerj.com>
2020-10-05 19:23:02 +1030

    Remove tag fetch (is it needed?)

--- .github/actions/prnumber.ps1
+++
@@ -1,9 +0,0 @@
-$repo = $env:GITHUB_REPOSITORY;
-$commitHash = $env:GITHUB_SHA;
-$url = "https://api.github.com/repos/$($repo)/commits/$($commitHash)/pulls";
-$json = Invoke-WebRequest $url -Headers @{Accept = "application/vnd.github.groot-preview+json"};
-$obj = ConvertFrom-Json $json;
-echo $url;
-echo $json;
-echo $obj.number;
-echo "::set-env name=PR_NUMBER::$($obj.number)";
