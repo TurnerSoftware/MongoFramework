218e3242097b9083943c52b6c81b3bf65edfcc8d
Turnerj <git@turnerj.com>
2020-10-05 16:23:38 +1030

    Output JSON for debugging

--- .github/actions/prnumber.ps1
+++ .github/actions/prnumber.ps1
@@ -3,5 +3,5 @@ $commitHash = $env:GITHUB_SHA;
 $url = "https://api.github.com/repos/$($repo)/commits/$($commitHash)/pulls";
 $json = Invoke-WebRequest $url -Headers @{Accept = "application/vnd.github.groot-preview+json"};
 $obj = ConvertFrom-Json $json;
-echo $obj;
+echo $json;
 echo "::set-env name=PR_NUMBER::$($obj.number)";
