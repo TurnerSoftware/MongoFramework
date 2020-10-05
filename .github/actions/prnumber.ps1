9dc0249db609df95e712872df6db9ac1d9a4120c
Turnerj <git@turnerj.com>
2020-10-05 16:07:05 +1030

    Updated how getting the PR number works

---
+++ .github/actions/prnumber.ps1
@@ -0,0 +1,6 @@
+$repo = $env:GITHUB_REPOSITORY;
+$commitHash = $env:GITHUB_SHA;
+$url = "https://api.github.com/repos/$($repo)/$($commitHash)/pulls";
+$json = Invoke-WebRequest $url -Headers @{Accept = "application/vnd.github.groot-preview+json"};
+$obj = ConvertFrom-Json $json;
+echo "::set-env name=GITHUB_PR::$($obj.number)";
