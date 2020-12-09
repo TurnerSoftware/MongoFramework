fa2df65fdc46b3be532d85eb1aff5e79c151a912
Turnerj <git@turnerj.com>
2020-12-09 21:10:38 +1030

    Add skip and take translators

---
+++ src/MongoFramework/Infrastructure/Linq/Translation/Translators/TakeTranslator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using MongoDB.Bson;
+
+namespace MongoFramework.Infrastructure.Linq.Translation.Translators
+{
+	public class TakeTranslator : IMethodTranslator
+	{
+		public IEnumerable<MethodInfo> GetSupportedMethods()
+		{
+			yield return TranslationHelper.GetMethodDefinition(() => Queryable.Take((IQueryable<object>)null, 0));
+		}
+
+		public BsonValue TranslateMethod(MethodCallExpression expression, IEnumerable<Expression> methodSuffixExpressions = default)
+		{
+			return new BsonDocument
+			{
+				{ "$limit", ExpressionTranslation.TranslateConstant(expression.Arguments[1]) }
+			};
+		}
+	}
+}
