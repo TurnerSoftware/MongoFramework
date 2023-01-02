f446709f8842413cb1a8b86406e7a784feef2e88
Turnerj <git@turnerj.com>
2023-01-02 17:52:29 +1030

    Updated debugger display of entity definitions

--- src/MongoFramework/Infrastructure/Mapping/EntityDefinition.cs
+++ src/MongoFramework/Infrastructure/Mapping/EntityDefinition.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace MongoFramework.Infrastructure.Mapping;
 
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public sealed record EntityDefinition
 {
 	public Type EntityType { get; init; }
@@ -12,8 +14,11 @@ public sealed record EntityDefinition
 	public IReadOnlyList<PropertyDefinition> Properties { get; init; } = Array.Empty<PropertyDefinition>();
 	public IReadOnlyList<IndexDefinition> Indexes { get; init; } = Array.Empty<IndexDefinition>();
 	public ExtraElementsDefinition ExtraElements { get; init; }
+
+	private string DebuggerDisplay => $"EntityType = {EntityType.Name}, Collection = {CollectionName}, Properties = {Properties.Count}, Indexes = {Indexes.Count}";
 }
 
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public sealed record PropertyDefinition
 {
 	public PropertyInfo PropertyInfo { get; init; }
@@ -28,32 +33,59 @@ public sealed record PropertyDefinition
 	{
 		PropertyInfo.SetValue(entity, value);
 	}
+
+	private string DebuggerDisplay => $"PropertyInfo = {PropertyInfo.Name}, ElementName = {ElementName}";
 }
 
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public sealed record IndexDefinition
 {
 	public IReadOnlyList<IndexPathDefinition> IndexPaths { get; init; }
 	public string IndexName { get; init; }
 	public bool IsUnique { get; init; }
 	public bool IsTenantExclusive { get; init; }
+
+	private string DebuggerDisplay => $"IndexName = {IndexName}, IndexPaths = {IndexPaths.Count}, IsUnique = {IsUnique}";
 }
 
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public sealed record IndexPathDefinition
 {
 	public string Path { get; init; }
 	public IndexType IndexType { get; init; }
 	public IndexSortOrder SortOrder { get; init; }
+
+	private string DebuggerDisplay => $"Path = {Path}, IndexType = {IndexType}, SortOrder = {SortOrder}";
 }
 
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public sealed record KeyDefinition
 {
 	public PropertyDefinition Property { get; init; }
 	public IEntityKeyGenerator KeyGenerator { get; init; }
+
+	private string DebuggerDisplay => $"PropertyInfo = {Property.PropertyInfo.Name}, ElementName = {Property.ElementName}";
 }
 
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public sealed record ExtraElementsDefinition
 {
 	public PropertyDefinition Property { get; init; }
 	public bool IgnoreExtraElements { get; init; }
 	public bool IgnoreInherited { get; init; }
+
+	private string DebuggerDisplay
+	{
+		get
+		{
+			if (IgnoreExtraElements)
+			{
+				return "IgnoreExtraElements = true";
+			}
+			else
+			{
+				return $"PropertyInfo = {Property.PropertyInfo.Name}, ElementName = {Property.ElementName}";
+			}
+		}
+	}
 }
